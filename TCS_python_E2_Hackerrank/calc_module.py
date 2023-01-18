5fc4a8e57a74a3422878aadabc8d05f38d1a9976
heylts <121068644+heylts@users.noreply.github.com>
2023-01-18 13:10:01 -0600

    Add files via upload

---
+++ TCS_python_E2_Hackerrank/calc_module.py
@@ -0,0 +1,11 @@
+def mul(a, b):
+    return a*b
+
+
+def div(a, b):
+    return a/b
+
+
+def even(num):
+    if num%2 == 0:
+        return True
---
+++ TCS_python_E2_Hackerrank/calc_module2.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a+b
+
+
+def sub(a, b):
+    return a-b
+
+
+def even(str1):
+    if len(str1)%2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
