30221d9e0825b804b7f01715696638c77146b32e
heylts <121068644+heylts@users.noreply.github.com>
2023-01-18 13:44:26 -0600

    Create optional1.py

---
+++ Optional assignments/optional1.py
@@ -0,0 +1,6 @@
+'''
+Write a function called "caught_speeding" where:
+1. Speed is 60 or less -> "no ticket"
+2. Speed is between 61 and 80 -> "small ticket"
+3. Speed is 81 or more -> "big ticket"
+'''
