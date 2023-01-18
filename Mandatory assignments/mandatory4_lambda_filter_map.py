3bb934e452e91e7f8b8111c2e665111951c116af
heylts <121068644+heylts@users.noreply.github.com>
2023-01-18 14:02:52 -0600

    Update and rename mandatory4_lambda_filter_map to mandatory4_lambda_filter_map.py

---
+++ Mandatory assignments/mandatory4_lambda_filter_map.py
@@ -0,0 +1,46 @@
+'''
+LAMBDA FILTER MAP
+
+LAMBDA_FILTER_MAP_REDUCE
+
+ 
+
+Do the following operations on the list l1 with 10 elements.
+
+ 
+
+1. Increment all the elements of the list by 1
+
+2. Extract the elements that are greater than 4 after incrementation.
+
+3. Print the sum of all the elements that are greater than 4.
+
+ 
+
+Input Format For Custom Testing
+Sample Case 0
+Sample Input For Custom Testing
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Sample Output
+
+56
+'''
+
+#CODE STUB
+
+from functools import reduce
+l1=[]
+for i in range(0,10):
+    l1.append(int(input()))
+
+# write your code here
